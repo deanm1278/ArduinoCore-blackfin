90698cac3b69ce2befdd91094fab6a2e51505548
dean <deanm1278@gmail.com>
2017-12-22 09:11:29 -0500

    interrupt stuff

--- cores/arduino/handlers.h
+++ cores/arduino/handlers.h
@@ -1,6 +1,13 @@
 #ifndef _HANDLERS_H
 #define _HANDLERS_H
 
+#define IRQ_MAX_PRIORITY 0xFF
+
+static inline void raiseIRQ(uint8_t num)
+{
+	SEC_Global->SEC0_RAISE.bit.SID = num;
+}
+
 //TODO: clean these up
 static inline void enableIRQ(uint8_t num)
 {
@@ -10,6 +17,12 @@ static inline void enableIRQ(uint8_t num)
 static inline void disableIRQ(uint8_t num)
 {
 	*(volatile uint32_t *)(((uint32_t)&SSI->SEC0_SCTL0) + (num << 3)) &= ~(0x05);
+}
+
+static inline void setIRQPriority(uint8_t num, uint8_t prio)
+{
+	*(volatile uint32_t *)(((uint32_t)&SSI->SEC0_SCTL0) + (num << 3)) &= ~(0xFF00);
+	*(volatile uint32_t *)(((uint32_t)&SSI->SEC0_SCTL0) + (num << 3)) |= (prio << 8);
 }
 
 static inline void noInterrupts( uint32_t *imask ){
