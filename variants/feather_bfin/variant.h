57026c742424d358c2a39714559c9f5880681dfe
dean <deanm1278@gmail.com>
2018-03-30 11:30:00 -0400

    fix variant pins

--- variants/feather_bfin/variant.h
+++ variants/feather_bfin/variant.h
@@ -24,8 +24,8 @@
 #define PIN_SERIAL_TX  		(15ul)
 
 // Serial1
-#define PIN_SERIAL1_RX       (0ul)
-#define PIN_SERIAL1_TX       (1ul)
+#define PIN_SERIAL1_RX       (1ul)
+#define PIN_SERIAL1_TX       (0ul)
 
 #define WIRE_INTERFACES_COUNT 1
 #define SPI_INTERFACES_COUNT 1
