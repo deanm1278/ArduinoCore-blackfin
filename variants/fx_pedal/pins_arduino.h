3e96eef688dbb30a1e11fd9420adc80c145dd93b
dean <deanm1278@gmail.com>
2018-06-08 19:21:42 -0400

    add fx pedal variant

--- variants/fx_pedal/pins_arduino.h
+++ variants/fx_pedal/pins_arduino.h
@@ -1,5 +1,5 @@
 /*
-  Copyright (c) 2015 Arduino LLC.  All right reserved.
+  Copyright (c) 2014-2015 Arduino LLC.  All right reserved.
 
   This library is free software; you can redistribute it and/or
   modify it under the terms of the GNU Lesser General Public
@@ -16,35 +16,6 @@
   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
-#include "Arduino.h"
-#include "handlers.h"
+// API compatibility
+#include "variant.h"
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-/*
- * System Core Clock is at 48mhz at reset (I think)
- */
-uint32_t SystemCoreClock=48000000ul ;
-
-void __init( void )
-{
-  //SEC_Global->SEC0_GCTL.bit.RESET = 1;
-  //SCI->SEC0_CCTL0.bit.RESET = 1;
-
-  //systick timer enable
-  TMR->TCNTL.bit.PWR = 1;
-  TMR->TCNTL.bit.AUTORLD = 1;
-  TMR->TSCALE.reg = 1; //decrement every 2 clock cycles
-  TMR->TPERIOD.reg = (VARIANT_MCK)/2000; //1 per ms
-  TMR->TCNTL.bit.EN = 1;
-
-  //enable interrupts
-  SEC_Global->SEC0_GCTL.bit.EN = 1;
-  SCI->SEC0_CCTL0.bit.EN = 1;
-}
-
-#ifdef __cplusplus
-}
-#endif
---
+++ variants/fx_pedal/variant.h
@@ -0,0 +1,96 @@
+#ifndef _VARIANT_BF706_PEDAL_
+#define _VARIANT_BF706_PEDAL_
+
+/*----------------------------------------------------------------------------
+ *        Definitions
+ *----------------------------------------------------------------------------*/
+
+/** Frequency of the board main oscillator */
+#define VARIANT_MAINOSC		(400000000ul)
+
+/** Master clock frequency */
+//TODO: this will change
+#define VARIANT_MCK			  (400000000ul)
+
+//this may need to be measured on each board
+#define VARIANT_SCLK0		  (100000000ul)
+
+#define L2_ORIGIN 			  0x08002000
+
+#define NUM_PORT 3
+
+#define LED_BUILTIN 	(13ul)
+
+//Serial
+#define PIN_SERIAL_RX  		(0ul)
+#define PIN_SERIAL_TX  		(1ul)
+
+#define WIRE_INTERFACES_COUNT 1
+#define SPI_INTERFACES_COUNT 2
+
+#define PERIPH_SPI SPI2
+
+//arduino SPI default is bfin peripheral SPI1
+#define PIN_SPI_SCK     14
+#define PIN_SPI_MISO    15
+#define PIN_SPI_MOSI    16
+#define PIN_SPI_D2      17
+#define PIN_SPI_D3      18
+#define PIN_SPI_SS      19
+
+#define SS PIN_SPI_SS
+
+#define PERIPH_SPI1 SPI1
+
+//arduino SPI1 is bfin peripheral SPI2 (confusing but helps compatibility)
+#define PIN_SPI1_SCK     20
+#define PIN_SPI1_MISO    21
+#define PIN_SPI1_MOSI    22
+#define PIN_SPI1_SS      23
+
+#define SS1 PIN_SPI1_SS
+
+//I2S
+#define PIN_BCLK 10
+#define PIN_FS 5
+#define PIN_AD0 9
+#define PIN_BD0 6
+
+
+/*----------------------------------------------------------------------------
+ *        Headers
+ *----------------------------------------------------------------------------*/
+
+#include "WVariant.h"
+
+#ifdef __cplusplus
+#include "Uart.h"
+#endif // __cplusplus
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif // __cplusplus
+
+
+extern __IO Portgroup *g_APorts[];
+extern __IO Dmagroup *DMA[];
+extern __IO Sportgroup *SPORT[];
+
+
+#ifdef __cplusplus
+}
+#endif
+
+/*----------------------------------------------------------------------------
+ *        Arduino objects - C++ only
+ *----------------------------------------------------------------------------*/
+
+#ifdef __cplusplus
+
+extern Uart Serial;
+extern Uart Serial1;
+
+#endif
+
+#endif
